2c4667f3a93811f64843d6907000a8845a189b94
Jamie Pennell <jamie.pennell@cloudcall.com>
2018-02-25 14:38:02 +0000

    permission handler update

--- src/permission-handler.js
+++ src/permission-handler.js
@@ -10,19 +10,20 @@ module.exports = class PermissionHandler {
         // will return ["usera/myUnit", "usera/unit:myotherunit", "userb/otherguysunit", "userc/another guys unit", "env:18836382"]
         // gameobject-detailed/username/name - subscribes to units, getting full detail, can only access own units
         // gameobject/username/name - subscribe to units, getting partial detail
-        console.log("PERMISSION CHECK: ", user, message);
-        if(user === "ADMIN") {
+        console.log("PERMISSION CHECK: ", user, message.data[0]);
+        if (user === "ADMIN") {
             return callback(null, true); //TODO this needs work
         }
-        if (message.data.startsWith("gameobject-detailed/")) {
-            if (message.data.startsWith("gameobject-detailed/" + user + "/") || message.data.startsWith("gameobject-detailed/env/")) {
+        if (message.data[0].startsWith("gameobject-detailed/")) {
+            if (message.data[0].startsWith("gameobject-detailed/" + user + "/") || message.data[0].startsWith("gameobject-detailed/env/")) {
                 callback(null, true);
             } else {
                 callback("Cannot view detailed view of another users unit", false);
             }
-        } else if (message.data.startsWith("gameobject/") || message.data.startsWith("room/")) {
+        } else if (message.data[0].startsWith("gameobject/") || message.data[0].startsWith("room/")) {
             callback(null, true);
+        } else {
+            callback("Not a valid endpoint", false);
         }
-        callback("Not a valid endpoint", false);
     }
 }
